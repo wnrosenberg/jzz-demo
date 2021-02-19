8b64ee99a9819ced43c87d35d32412e9a1d9787b
Will Rosenberg <will.rosenberg@gmail.com>
2021-02-19 16:17:23 -0500

    Created methods for generating the MIDI messages for all, col, row, and individual pad color change.

--- index.js
+++ index.js
@@ -4,6 +4,56 @@ import JZZ from 'jzz';
 
 let outputPort = null;
 
+// Get a MIDI msg for updating pad colors.
+const getPadChangeMsg = function(pads) {
+	const header = [0xf0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x0a];
+	let padData = [];
+	const terminator = [0xf7];
+	pads.forEach((pad, i)=>{
+		padData.push(...pad);
+	});
+	return JZZ.MIDI([...header, ...padData, ...terminator]);
+};
+
+const getColChangeMsg = function(col) {
+	const header = [0xf0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x0c];
+	let padData = [];
+	const terminator = [0xf7];
+
+	// if pads is an array of pads, do a forEach.
+	// if (pads.length && 'object' === typeof pads[0]) {
+	// 	pads.forEach((pad, i)=>{
+	// 		padData.push(...pad);
+	// 	});
+	// } else {
+		padData.push(...col);
+	// }
+	return JZZ.MIDI([...header, ...padData, ...terminator]);
+};
+
+const getRowChangeMsg = function(col) {
+	const header = [0xf0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x0d];
+	let padData = [];
+	const terminator = [0xf7];
+
+	// if pads is an array of pads, do a forEach.
+	// if (pads.length && 'object' === typeof pads[0]) {
+	// 	pads.forEach((pad, i)=>{
+	// 		padData.push(...pad);
+	// 	});
+	// } else {
+		padData.push(...col);
+	// }
+	return JZZ.MIDI([...header, ...padData, ...terminator]);
+};
+
+const getAllChangeMsg = function(color) {
+	const header = [0xf0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x0e];
+	const terminator = [0xf7];
+	return JZZ.MIDI([...header, ...color, ...terminator]);
+};
+
+
 // Init the app after JZZ is refreshed
 const initApp = function() {
 	
@@ -13,14 +63,66 @@ const initApp = function() {
 	// Assign the port that we'll use for the ouput.
 	outputPort = JZZ().openMidiOut(1);
 
-	// try some sysex magic and print response
-	let message = JZZ.MIDI([
-		0xf0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x0a, // sysex to adjust LEDs
-		0x63, 6, // led index, color
-		0xf7 // sysex terminator
-	]);
-	let response = outputPort.send(message);
-	console.log('Sent MIDI message', message, 'and got response', response);
+	// ----------------
+
+	let data;
+	let colors;
+	let message;
+	let response;
+
+	// --------------------------------------
+
+	// Light up all pads with a color
+	data = [100];
+	message = getAllChangeMsg(data);
+
+	// Send the message to the launchpad.
+	console.log('Sending MIDI message', message);
+	response = outputPort.send(message).then(
+		()=>{console.log('Worked')},
+		()=>{console.log('Error')}
+	);
+
+	// ----------------------------------
+
+	// Light up individual pads
+	data = [ [0x63, 6], [0x5b, 17], [0x5c, 27], [0x5d, 37] ];
+	message = getPadChangeMsg(data);
+
+	// Send the message to the launchpad.
+	console.log('Sending MIDI message', message);
+	response = outputPort.send(message).then(
+		()=>{console.log('Worked')},
+		()=>{console.log('Error')}
+	);
+
+	// ---------------------------
+
+	// Light up a single column with colors 0 through 9.
+	colors = [48,49,0,51,52,0,59,60,0,62]; // column from bottom(0) to top(9)
+	data = [4, ...colors];
+	message = getColChangeMsg(data);
+
+	// Send the message to the launchpad.
+	console.log('Sending MIDI message', message);
+	response = outputPort.send(message).then(
+		()=>{console.log('Worked')},
+		()=>{console.log('Error')}
+	);
+
+	// ---------------------------
+
+	// Light up a single column with colors 0 through 9.
+	colors = [68,69,70,71,72,88]; // row from left(0) to right(9)
+	data = [4, ...colors];
+	message = getRowChangeMsg(data);
+
+	// Send the message to the launchpad.
+	console.log('Sending MIDI message', message);
+	response = outputPort.send(message).then(
+		()=>{console.log('Worked')},
+		()=>{console.log('Error')}
+	);
 
 };
 

910d6c114cee49d7ed5dac0f308b8b26478c6374
Will Rosenberg <will.rosenberg@gmail.com>
2021-02-19 15:28:28 -0500

    Added refresh button. Basic MIDI msg sent.

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 import JZZ from 'jzz';
 
+////////////////////////////////
 
-console.log('This is my app');
+let outputPort = null;
 
- JZZ().or('Cannot start MIDI engine!')
-       .openMidiOut().or('Cannot open MIDI Out port!')
-       .wait(500).send([0x90,60,127])
-       .wait(500).send([0x90,64,127])
-       .wait(500).send([0x90,67,127])
-       .wait(500).send([0x90,72,127])
-       .wait(1000).send([0x90,60,0]).send([0x90,64,0]).send([0x90,67,0]).send([0x90,72,0])
-       .and('thank you!');
+// Init the app after JZZ is refreshed
+const initApp = function() {
+	
+	// Connect the refresh button an action
+	document.getElementById('refresh').addEventListener('click',function(){refreshJazz()});
+
+	// Assign the port that we'll use for the ouput.
+	outputPort = JZZ().openMidiOut(1);
+
+	// try some sysex magic and print response
+	let message = JZZ.MIDI([
+		0xf0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x0a, // sysex to adjust LEDs
+		0x63, 6, // led index, color
+		0xf7 // sysex terminator
+	]);
+	let response = outputPort.send(message);
+	console.log('Sent MIDI message', message, 'and got response', response);
+
+};
+
+////////////////////////////////
+
+// Refresh the JZZ plugin after connect/disconnect of midi gear
+const refreshJazz = function() {
+	console.log('Refresh btn clicked.');
+	JZZ().refresh().then(function(){
+		console.log('JZZ Refreshed: ', JZZ().info());
+		initApp();
+	});
+};
+
+//////////////////////////////
+
+// Init the JZZ plugin and trigger app init
+const initJazz = function() {
+	JZZ({sysex: true}).or('Cannot start MIDI engine!').and(function(){
+		console.log('JZZ initialized', JZZ().info());
+		initApp();
+	})
+};
+
+///////////////////////////////
+
+// init on domready and lets goooooooooooo
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    initJazz();
+  }
+}
